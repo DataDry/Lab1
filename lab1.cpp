188122e9b101757350cc7cb5e4a8e729285453b9
DataDry <adolgij.unium@gmail.com>
2023-09-27 12:06:26 +0300

    Added float type support Added int/float selection

--- lab1.cpp
+++ lab1.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <Windows.h>
 using namespace std;
-void NumberAsBinary(int value)
+void IntToBin(int value)
 {
 	unsigned int order = 32; // Количество разрядов
 	unsigned int mask = 1 << order - 1; // Маска побитового сравнения
@@ -19,18 +18,7 @@ void NumberAsBinary(int value)
 		}
 	}
 }
-/*void FloatToBInary(float value) {
-	union {
-		int integer = 127;
-		float floatB;
 
-	};
-	for (int i = 0; i < order; i++) {
-		cout << (integerA & mask) ? 1 : 0);
-		integerA = integerA << 1;
-		if (!i) { cout << ' '; }
-	}
-}*/
 void showsize(){
     cout<< "Size of int: "<<sizeof(int)<< "\n";
     cout<< "Size of short int: "<<sizeof(short int)<< "\n";
@@ -42,19 +30,61 @@ void showsize(){
     cout<< "Size of bool: "<<sizeof(bool)<< "\n";
 }
 
+void FloatToBin(float deg)
+{
+	union
+	{
+		float a;
+		int b;
+	};
+	a = deg;
+	unsigned int mask;
+	unsigned int bites = 32; mask = 1 << bites - 1;
+	for (int i = 1; i <= bites; i++)
+	{
+		putchar(b & mask ? '1' : '0');
+		b <<= 1;
+		if (i == 1 || i == 2 || i == 9)
+			cout << " ";
+	}
+}
+
+void IntAction() {
+	int deg;
+	cout << "Enter non-digit to exit" << "\n" << "Enter integer" << "\n\n";
+	cin >> deg;
+	while (deg != NULL and deg < INT_MAX and deg > INT_MIN) {
+		IntToBin(deg);
+		cout << "\n" << "Address: " << &deg << "\n\n";
+		cin >> deg;
+	}
+}
+void FloatAction() {
+	float deg;
+	cout << "Enter non-digit to exit" << "\n" << "Enter float" << "\n\n";
+	cin >> deg;
+	while (deg != NULL and deg < INT_MAX and deg > INT_MIN) {
+		FloatToBin(deg);
+		cout << "\n" << "Address: " << &deg << "\n\n";
+		cin >> deg;
+	}
+}
+
+void ChoseAction() {
+	cout << "\nChose the desired type of number:\n1. Integer\n2. Float\n\n";
+	unsigned short int NumberTypeInput;
+	cin >> NumberTypeInput;
+	switch (NumberTypeInput) {
+	case(1): system("cls"); IntAction(); break;
+	case(2): system("cls"); FloatAction(); break;
+	default: ChoseAction(); break;
+	}
+}
 
 int main()
 {
     showsize();
-    cout<<"\n\n";
-    int deg;
-	cout << "\n" << "Enter non-digit to exit" << "\n" << "Enter integer" << "\n\n";
-	cin >> deg;
-
-	while (deg != NULL and deg < INT_MAX and deg > INT_MIN) {
-		NumberAsBinary(deg);
-		cout << "\n" << "Address: " << &deg << "\n\n";
-		cin >> deg;
-	}
-	
+    cout<<"\n";
+    
+	ChoseAction();
 }
